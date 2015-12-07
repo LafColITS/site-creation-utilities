49784fc2a678c70f815818528a346671e18ad17d
Charles Fulton <fultonc@lafayette.edu>
2015-12-07 15:51:54 +0000

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Site-creation-utilities ===
+Contributors: mackensen
+Tags: wpmu
+Requires at least: 4.5
+Tested up to: 4.5
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin improves the Multisite site creation process by eliminating undesired features.
+
+== Description ==
+
+This plugin eliminates two undesired features from Multisite site creation:
+
+- It suppresses the welcome email sent to new site administrators on site creation
+- It cancels the site creation process if the given email does not exist, instead of creating a local user
+
+== Installation ==
+
+1. Upload the `site-creation-utilities` folder to the `/wp-content/plugins/` directory
+1. Network activate the plugin through the 'Plugins' menu in the WordPress Network Admin
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release.
---
+++ site-creation-utilities.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Site Creation Utilities
+ * Version: 0.1-alpha
+ * Description: This plugin improves the Multisite site creation process by eliminating undesired features.
+ * Author: Charles Fulton
+ * Author URI: https://www.lafayette.edu
+ * Plugin URI: https://git.lafayette.edu/wordpress/site-creation-utilities
+ * Text Domain: site-creation-utilities
+ * Domain Path: /languages
+ * @package Site-creation-utilities
+ */
+
+class Site_Creation_Utilities {
+
+	// Prevents the creation of a local user account when creating a new site.
+	public static function disable_user_creation( $args ) {
+		wp_die( __( 'The requested user does not exist and automatic user creation is disabled', 'site-creation-utilities' ) );
+	}
+
+	// Suppress welcome email on site creation.
+	public static function disable_welcome_email( $blog_id, $user_id, $password, $title, $meta ) {
+		return false;
+	}
+}
+
+add_action( 'pre_network_site_new_created_user', array( 'Site_Creation_Utilities', 'disable_user_creation' ) );
+add_filter( 'wpmu_welcome_notification', array( 'Site_Creation_Utilities', 'disable_welcome_email' ) );
