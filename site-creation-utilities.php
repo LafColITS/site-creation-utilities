2b1e8d4c4b0f497de521d3014fa42ed34cd0f8c5
Charles Fulton <fultonc@lafayette.edu>
2017-02-06 12:48:28 -0500

    Flush rewrite rules on site creation

--- site-creation-utilities.php
+++ site-creation-utilities.php
@@ -22,7 +22,15 @@ class Site_Creation_Utilities {
 	public static function disable_welcome_email( $blog_id, $user_id, $password, $title, $meta ) {
 		return false;
 	}
+
+	// Flush rewrite rules on site creation.
+	public static function flush_rewrite_rules( $blog_id, $user_id, $domain, $path, $site_id, $meta ) {
+		switch_to_blog( $blog_id );
+		update_option( 'rewrite_rules', array() );
+		restore_current_blog();
+	}
 }
 
 add_action( 'pre_network_site_new_created_user', array( 'Site_Creation_Utilities', 'disable_user_creation' ) );
 add_filter( 'wpmu_welcome_notification', array( 'Site_Creation_Utilities', 'disable_welcome_email' ), 10, 5 );
+add_action( 'wpmu_new_blog', array( 'Site_Creation_Utilities', 'flush_rewrite_rules' ), 10, 6 );
